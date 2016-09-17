b3f9cadf5e0cb64418382a788c35794cf181a66c
Rajiv Thamburaj <rjt2132@columbia.edu>
2016-09-17 17:29:53 -0400

    Add our Player class.

---
+++ g9/Player.java
@@ -0,0 +1,18 @@
+package slather.g9;
+
+import slather.sim.Cell;
+import slather.sim.Point;
+import slather.sim.Move;
+import slather.sim.Pherome;
+import java.util.*;
+
+
+public class Player implements slather.sim.Player {
+    
+    public void init(double d, int t) {
+    }
+
+    public Move play(Cell player_cell, byte memory, Set<Cell> nearby_cells, Set<Pherome> nearby_pheromes) {
+    }
+
+}
